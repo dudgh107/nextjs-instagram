2ac5f10c8a1ca70fb7a243f5eb0bf865c14a46f7
Youngho Jung <ntyoungho@nate.com>
2024-06-19 01:57:22 +0900

    메인화면 출력 아이디 원복

--- src/service/posts.ts
+++ src/service/posts.ts
@@ -17,7 +17,9 @@ const simplePostProjection = `
 export async function getFollowingPostsOf(username: string) {
     
     return client.fetch(
-        `*[_type =="post" 
+        `*[_type =="post" && author->username == "${username}"
+            || author._ref in *[_type == "user" && username == "${username}"].following[]._ref
+
             ] | order(_createdAt desc){${simplePostProjection}}`
     ).then(mapPosts);
 }
